0cb73e94c107efe174776f78faccacf8ad63291f
K Sasisekhara Preetam <kspreetam2608@gmail.com>
2026-06-29 14:11:46 +0530

    Delete SmartSociety.Domain directory

--- SmartSociety.Domain/Enums/ComplaintStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace SmartSociety.Domain.Enums;
-
-public enum ComplaintStatus
-{
-    Open = 1,
-    Resolved = 2,
-    Closed = 3
-}
--- SmartSociety.Domain/Enums/UserRole.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartSociety.Domain.Enums;
-
-public enum UserRole
-{
-    Admin = 1,
-    Resident = 2,
-    SecurityStaff = 3,
-    Owner = 4
-}
